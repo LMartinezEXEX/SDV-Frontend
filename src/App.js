6d3d8a137b37a7b94bbbafcf89962c780ac973e9
Agustin Chapuis <aguschapuis>
2020-10-19 11:57:40 -0300

    navigator without implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './assets/css/App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+          <li> <a href="#">Login</a>  </li>
+          <li> <a href="#">Lobby</a> </li>
+          <li> <a href="#">Pre-game</a> </li>
+          <li> <a href="#">games</a> </li>
+      </nav>
       <div className="Initial">
         <h1>Secret Voldemort</h1>
       </div>
