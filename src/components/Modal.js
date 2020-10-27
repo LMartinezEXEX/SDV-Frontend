004a4f8a28caa073d8e53bf3758bb2c23cdac83f
aguschapuis <aguschapuis>
2020-10-26 23:45:24 -0300

    votation window added with two buttons to accept or decline

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import '../assets/css/modal.css'
 import ChargeTable from '../components/game/chargesTable'
+import Votation from '../components/game/votation'
 
 // a switch to know the information that will be displayed in the open window
 const windowData = (children) => { 
     switch(children){
         case "Cargos": return(<ChargeTable/>)
+        case "Votar": return(<Votation/>)
         default: return children
     }
 }
