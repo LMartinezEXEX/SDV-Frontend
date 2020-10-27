4eded45d0ec9dda7dd72288f42dfd2f14ea589e7
Francisco Joray <franjoray@mi.unc.edu.ar>
2020-10-27 05:16:33 -0300

    corrections to register form

--- src/components/home/register/RegisterForm.jsx
+++ src/components/home/register/RegisterForm.jsx
@@ -1,75 +1,140 @@
-import ReactDOM from 'react-dom';
-import React,{ Component , useState} from 'react'
+import React, { useState } from 'react'
+import Input from './Input'
 
 
 const RegisterForm = () => {
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [pass, setPass] = useState('');
-  const [avatar, setAvatar] = useState(0);
-  const avatars = ["Harry","Ron","Hermione","Snape","Draco","Lucius","Umbridge","Voldemort"]
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [avatar, setAvatar] = useState(0);
+    const [passwordError, setPasswordError] = useState(false);
+   // const [isLogin, setIsLogin] = useState(false);
+   // const [hasError, setHasError] = useState(false);
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  }
+    const avatars = ["Harry","Ron","Hermione","Snape","Draco","Lucius","Umbridge","Voldemort"]
 
-  const handleNameChange = (event) => {
-    setUsername(event.target.value);
-  }
+    function handleChange(name, value) {
+        if (name === 'email') {
+            setEmail(value)
+        } else if (name === 'username') {
+            setUsername(value)
+        } else {
+            if (value.length < 8) {
+                setPasswordError(true);
+            } else {
+                setPasswordError(false);
+                setPassword(value)
+            }
+        }
+    }
 
-  const handlePassChange = (event) => {
-    setPass(event.target.value);
-  }
+    /*function isMatch(param) {
+        if (param.email.length > 0 && param.password > 0) {
+            if (param.email === 'sopa@verduras' && param.password === '1234') {
+                const { email, password } = param;
+                let ac = { email, password }
+                let account = JSON.stringify(ac);
+                localStorage.setItem('account', account);
+              //  setIsLogin(true);
+            } else {
+                //setIsLogin(false);
+                setHasError(true);
+            }
+            //setIsLogin(false);
+            setHasError(true);
+        }
+    }
+    */
+    const handleSubmit = (event) => {
+        let account = { email, username, password, avatar }
+        if (account) {
+            //isMatch(account);
+            console.log('account',account);
+        }
+        event.preventDefault(); // esto VER 
+    }
 
-  const handleSubmit = (event) => {
-    alert('Datos Ingresados:' +
-          '\nEmail: ' + email +
-          '\nNombre de Usuario: ' + username +
-          '\nContraseña: ' + pass +
-          '\nAvatarID:' + avatar +
-          '\nCreation Date:\n' + new Date());
-    event.preventDefault();
-  }
-  
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    
+    return (
         <div>
-          <label>
-            Email:
-            <input type="email" value={email} required="true" onChange={handleEmailChange} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Nombre de Usuario:
-            <input type="text" value={username} required="true" onChange={handleNameChange} />
-          </label>
-        </div>
-        <div>
-          <label>
-            Contraseña:
-            <input type="password" maxlength="8" required="true" value={pass} onChange={handlePassChange} />
-          </label>
-        </div>
-        <div>
-          Avatar: {avatars[avatar]}
-        </div>
+            <form className='register-container' onSubmit={handleSubmit}>
+                <div> {/* COMMENT 
+                    { hasError &&
+                        <label>
+                            Su contraseña o usuario son incorrectos,
+                            o no existen en nuestra plataforma
+                        </label>
+                    }*/}
+                    <label>
+                        Email:
+                        <Input attribute={{
+                            id: 'email',
+                            name: 'email',
+                            type: 'email',
+                            required: 'true',
+                            placeholder: 'Ingrese su email'
+                        }}
+                            handleChange={handleChange}
+                            param={passwordError}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Nombre de Usuario:
+                        <Input attribute={{
+                            id: 'username',
+                            name: 'username',
+                            type: 'text',
+                            required: 'true',
+                            placeholder: 'Ingrese su nombre de usuario'
+                        }}
+                            handleChange={handleChange}
+                        />
 
-        <input type="submit" value="¡Regístrate!" />
-      </form>
-      <div>
-        <button className="harry" onClick={() => setAvatar(0)} />
-        <button className="ron" onClick={() => setAvatar(1)} />
-        <button className="hermione" onClick={() => setAvatar(2)} />
-        <button className="snape" onClick={() => setAvatar(3)} />
-        <button className="draco" onClick={() => setAvatar(4)} />
-        <button className="lucius" onClick={() => setAvatar(5)} />
-        <button className="umbridge" onClick={() => setAvatar(6)} />
-        <button className="voldemort" onClick={() => setAvatar(7)} />
-      </div>
-    </div>
-  );
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Contraseña:
+                        <Input attribute={{
+                            id: 'password',
+                            name: 'password',
+                            type: 'password',
+                            required: 'true',
+                            placeholder: 'Ingrese su contraseña'
+                        }}
+                            handleChange={handleChange}
+                        />
+
+                    </label>
+                </div>
+                <div>
+                    Avatar: {avatars[avatar]}
+                </div>
+
+                <input type="submit" value="¡Regístrate!" />
+           
+            </form>
+
+            <div>
+                <button className="harry" onClick={() => setAvatar(0)} />
+                <button className="ron" onClick={() => setAvatar(1)} />
+                <button className="hermione" onClick={() => setAvatar(2)} />
+                <button className="snape" onClick={() => setAvatar(3)} />
+            <div>
+            </div>
+                <button className="draco" onClick={() => setAvatar(4)} />
+                <button className="lucius" onClick={() => setAvatar(5)} />
+                <button className="umbridge" onClick={() => setAvatar(6)} />
+                <button className="voldemort" onClick={() => setAvatar(7)} />
+            </div>
+
+        </div>
+    )
 }
-  
+
 export default RegisterForm;
+
+//useEffect evitamos que se renderice muchas veces! FIJARSE
+
