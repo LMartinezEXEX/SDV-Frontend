3b73e232f4520d5b6d1e6204e244c8bba3ea57c2
Francisco Joray <franjoray@mi.unc.edu.ar>
2020-10-28 01:51:40 -0300

    signup pop-up with register-form

--- src/components/home/SignUp.js
+++ src/components/home/SignUp.js
@@ -1,20 +1,21 @@
-
 import React, { useState } from 'react'
 import Modal from '../Modal'
+import RegisterForm from './register/RegisterForm'
+import '../../assets/css/RegisterForm.css'
 
-const SignUp= () => {
-  const [isOpen, setIsOpen] = useState(false)
-  return (
-    <>
-      <div>
-        <button className= "app-btn" onClick={() => setIsOpen(true)}> Registrarse </button>
-
-        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          REGISTER FORM
-        </Modal>
-      </div>
-    </>
-  )
+const LogIn= () => {
+    const [isOpen, setIsOpen] = useState(false)
+    
+    return (
+  
+        <div >
+          <button className= "app-btn" onClick={() => setIsOpen(true)}> Ingresar </button>
+          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            <RegisterForm />
+          </Modal>
+        </div>
+    
+    )
 }
 
-export default SignUp;
+export default LogIn;
