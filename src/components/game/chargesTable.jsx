bdaf5f60b23f0532cc62b5e9f2044432b73cce7e
aguschapuis <aguschapuis>
2020-10-28 00:25:04 -0300

    endpoint added to charge table and i started the implementation of the votation with the buttons

--- src/components/game/chargesTable.jsx
+++ src/components/game/chargesTable.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../assets/css/game.css'
+import axios from 'axios';
 
-function ChargesTable() {
+
+const ChargesTable = () => {
+    const [actualMinister, setNextMinister] = useState(0)
+    const setMinistro = async () => {
+        await axios.put("http://127.0.0.1:8000/game/1/select_MM", {
+        method:'PUT',
+        headers: {
+            'accept': 'application/json',
+        }}).then(res => {
+            setNextMinister(res.data.candidate_minister_id)
+        })
+    }
+
     return (
         <div className="chargeTable">
             <ul>
-                <li>Ministro: {"-"} </li>
+                <button onClick={setMinistro}>Elegir ministro</button>
+                <li>Ministro: {actualMinister} </li>
                 <li>Presidente: {"-"} </li>
             </ul>
         </div>
@@ -13,3 +27,5 @@ function ChargesTable() {
 }
 
 export default ChargesTable
+
+// {"data":{"candidate_minister_id":1},"status":200,"statusText":"OK",
--- src/components/game/votation.jsx
+++ src/components/game/votation.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../assets/css/votation.css'
+import axios from 'axios'
 
-function Votation() {
+const Votation = () => {
+    const [vote, setVote] = useState(false) 
+    const [PlayerVoting, nextPlayer] = useState(2)
+    const [votationOpen, enableVotation] = useState(true)
+    const uploadVote = async (decition) => {
+        setVote(decition)
+        axios.put('http://127.0.0.1:8000/game/1/vote', {
+            id: PlayerVoting,
+            vote: vote
+        })
+        .then(res => {
+            nextPlayer(PlayerVoting+1)
+            if (PlayerVoting >= 5) {enableVotation(false)}
+        });
+    } 
+
     return (
         <div>
             <h3>Candidatos</h3>
             <ul>
                 <li>Ministro: -</li>
                 <li>Presidente: -</li>
+                <li>Jugador Votando: {PlayerVoting}</li>
             </ul>
-            <button className="votationButton" onClick={() => {alert("Lumus")}}>
+            <button className="votationButton" disabled={!votationOpen} onClick={() => {uploadVote(true)}}>
                 <i class="far fa-thumbs-up fa-3x"></i></button>
-            <button className="votationButton" onClick={() => {alert("Nox")}}>
+            <button className="votationButton" disabled={!votationOpen} onClick={() => {uploadVote(false)}}>
                 <i class="far fa-thumbs-down fa-3x"></i></button>
         </div>
     )
