874a70d8c0e9377afe1a9c04b5531bc551cf2846
Francisco Joray <franjoray@mi.unc.edu.ar>
2020-11-01 16:02:21 -0300

    join game endpoint

--- src/components/lobby/JoinGame.js
+++ src/components/lobby/JoinGame.js
@@ -1,14 +1,17 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Modal from '../Modal'
+import JoinForm from './join/JoinForm'
 
 const JoinGame= () => {
-   
-    
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
   
         <div >
-          <button className= "app-btn" onClick={() => alert}> Join Game </button>
-       
+            <button className= "app-btn" onClick={() => setIsOpen(true)}> Join Game </button>
+            <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+                <JoinForm />
+            </Modal>
         </div>
     
     )
