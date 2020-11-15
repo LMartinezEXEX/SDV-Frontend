4c292bf7909ba74737460efd5698d7ace68f8205
reydc <daniel.rey.consultas@gmail.com>
2020-11-15 02:33:03 -0300

    PopUp mejorado para que acepte una función que pase información al estado

--- src/components/game/PopUp.js
+++ src/components/game/PopUp.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import Modal from '../Modal'
 
 const PopUp= (props) => {
-    const { type, enableButton } = props
+    const { type, enableButton, handleState } = props
     const [isOpen, setIsOpen] = useState(false)
     
     return (
         <div>
             {(enableButton)
             ?(
-            <button className="app-btn" id="gameButton"
-                onClick={() => { setIsOpen(true) }}> {type} </button>
+                (handleState)
+                ?(
+                    <button className="app-btn" id="gameButton"
+                     onClick={() => { handleState(); setIsOpen(true) }}> {type} </button>
+                ):(
+                    <button className="app-btn" id="gameButton"
+                     onClick={() => { setIsOpen(true) }}> {type} </button>
+                )
             ):(
                 <button className="app-btn" id="gameButton"
                 onClick="" > {type} </button>                
