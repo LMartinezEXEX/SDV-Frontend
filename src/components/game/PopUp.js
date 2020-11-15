6e50faf5ae4b0ed08823c983cb792fb06c732ec8
reydc <daniel.rey.consultas@gmail.com>
2020-11-14 23:40:28 -0300

    Habilitación en PopUp, pasando un prop

--- src/components/game/PopUp.js
+++ src/components/game/PopUp.js
@@ -2,13 +2,19 @@ import React, { useState } from 'react'
 import Modal from '../Modal'
 
 const PopUp= (props) => {
-    const {gameState, gameUpdater, type} = props
+    const { type, enableButton } = props
     const [isOpen, setIsOpen] = useState(false)
     
     return (
-        <div >
-            <button className= "app-btn" id="gameButton"
-                onClick={() => {setIsOpen(true)}}> {type} </button>
+        <div>
+            {(enableButton)
+            ?(
+            <button className="app-btn" id="gameButton"
+                onClick={() => { setIsOpen(true) }}> {type} </button>
+            ):(
+                <button className="app-btn" id="gameButton"
+                onClick="" > {type} </button>                
+            )}
             <Modal open={isOpen} setIsOpen={setIsOpen} children={type}
                 onClose={() => setIsOpen(false)}/>
         </div>
