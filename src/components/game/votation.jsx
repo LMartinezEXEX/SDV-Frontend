26d09700b49bda0d68ae46ec081e6851f0e371c6
reydc <daniel.rey.consultas@gmail.com>
2020-11-16 20:41:19 -0300

    class por className

--- src/components/game/votation.jsx
+++ src/components/game/votation.jsx
@@ -34,10 +34,10 @@ const Votation = (props) => {
                     <li>Candidato Director: {candidateDirector} </li>
                 </ul>
                 <button className="votationButton" onClick={() => {uploadVote(true)}}>
-                    <i class="far fa-thumbs-up fa-3x"></i>
+                    <i className="far fa-thumbs-up fa-3x"></i>
                 </button>
                 <button className="votationButton" onClick={() => {uploadVote(false)}}>
-                    <i class="far fa-thumbs-down fa-3x"></i>
+                    <i className="far fa-thumbs-down fa-3x"></i>
                 </button>
             </div>
         )
