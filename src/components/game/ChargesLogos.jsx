773be2268ad4283fbabbeb539ebc46c8cd4950f6
aguschapuis <aguschapuis>
2020-11-23 12:09:34 -0300

    changes maked in charge displayer

--- src/components/game/ChargesLogos.jsx
+++ src/components/game/ChargesLogos.jsx
@@ -15,3 +15,11 @@ export const DirectorLogo = () => {
         </div>
     )
 }
+
+export const VoldemortLogo = () => {
+    return(
+        <div className="ChargeLogo">
+            V
+        </div>
+    )
+}
